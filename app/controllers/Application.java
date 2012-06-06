6c5df8bde7d2d84ad67079fc61dae7a053826600
Johannes Unterstein <unterstein@me.com>
2012-06-06 11:33:58 +0200

    enabled routes and sign in components

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,9 +6,33 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+
+    public static Result index() {
+        return ok(login.render());
+    }
+
+    public static Result loginUser() {
+        // final Form<> registerForm = Controller.form(Register.class).bindFromRequest();
+        return ok();
+    }
+
+    public static Result registerUser() {
+        // final Form<> registerForm = Controller.form(Register.class).bindFromRequest();
+        return ok();
+    }
+
+    public static Result registerPanel() {
+        return ok();
+    }
+
+    // -- Javascript routing
+
+    public static Result jsRoutes() {
+        response().setContentType("text/javascript");
+        return ok(Routes.javascriptRouter("jsRoutes",
+                controllers.routes.javascript.Application.loginUser(),
+                controllers.routes.javascript.Application.registerUser(),
+                controllers.routes.javascript.Application.registerPanel()
+                ));
+    }
 }
