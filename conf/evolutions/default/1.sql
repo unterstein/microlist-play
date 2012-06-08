a6df7e5653ae66b172619a1657837076996dc8e3
Johannes Unterstein <unterstein@me.com>
2012-06-08 07:49:18 +0200

    added evolutions file

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,24 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table user (
+  id                        bigint auto_increment not null,
+  password                  varchar(255),
+  email                     varchar(255),
+  constraint uq_user_email unique (email),
+  constraint pk_user primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table user;
+
+SET FOREIGN_KEY_CHECKS=1;
+
