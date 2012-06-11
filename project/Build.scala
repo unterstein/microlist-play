ef609946bf090b1ade5443f2e6ec2cb2f92f4523
Johannes Unterstein <unterstein@me.com>
2012-06-11 12:15:13 +0200

    introduced war plugin

--- project/Build.scala
+++ project/Build.scala
@@ -10,12 +10,14 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       "mysql" % "mysql-connector-java" % "5.1.20",
       "com.github.twitter" % "bootstrap" % "2.0.3",
-      "com.jquery" % "jquery" % "1.7.1"
+      "com.jquery" % "jquery" % "1.7.1",
+      "com.github.play2war" %% "play2-war-core" % "0.4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       resolvers += ("Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository"),
-      resolvers += ("webjars" at "http://webjars.github.com/m2")
+      resolvers += ("webjars" at "http://webjars.github.com/m2"),
+      resolvers += ("Play2war plugins release" at "http://repository-play-war.forge.cloudbees.com/release/")
     )
 
 }
