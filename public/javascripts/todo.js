58bcb7c8d7d9197336d4c78f6a82bfed248afba8
Johannes Unterstein <unterstein@me.com>
2012-06-15 07:22:56 +0200

    introduced new checkbox styles

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -77,33 +77,3 @@ function replaceElement(element, data) {
     $(element).replaceWith(data);
     doAfterAjaxHandling();
 }
-
-/**
- * Checkboxes from:
- * http://webdesign.maratz.com/lab/fancy-checkboxes-and-radio-buttons/jquery.html
- */
-function setupLabel() {
-    if ($('.label_check input').length) {
-        $('.label_check').each(function(){ 
-            $(this).removeClass('c_on');
-        });
-        $('.label_check input:checked').each(function(){ 
-            $(this).parent('label').addClass('c_on');
-        });                
-    };
-    if ($('.label_radio input').length) {
-        $('.label_radio').each(function(){ 
-            $(this).removeClass('r_on');
-        });
-        $('.label_radio input:checked').each(function(){ 
-            $(this).parent('label').addClass('r_on');
-        });
-    };
-};
-$(document).ready(function(){
-    $('body').addClass('has-js');
-    $('.label_check, .label_radio').click(function(){
-        setupLabel();
-    });
-    setupLabel(); 
-});
